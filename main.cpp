aa1c72bf32a7e729618dd532238bb005515ea912
ray <159608790+ray0hku@users.noreply.github.com>
2024-04-23 20:14:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
+#include <SFML/Graphics.hpp>
 
-int main(){
-  
-  return 0
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Window Title");
+    
+    // Main game loop
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+        // Update the game logic
+        
+        window.clear(); // Clear the window
+        
+        // Draw objects to the window
+        
+        window.display(); // Display the window
+    }
+    
+    return 0;
 }
