a4bc86923e9b88a476093bd1ad870ab50e7f4063
DearFirefly <159635690+DearFirefly@users.noreply.github.com>
2024-04-24 17:12:48 +0800

    Update Difference.cpp

--- Difference.cpp
+++ Difference.cpp
@@ -1,24 +1,5 @@
 #include "header.h" //header file
 
-void correct_position(char correct_character) //printing right characters with right position, indicated by green colour
-{
-    printf("\033[32m[ "); 
-    printf("\033[32m%c", correct_character);
-    printf("\033[32m ]\033[0m");
-}
-void wrong_position(char wrong_position) //printing right characters with wrong position, indicated by yellow colour
-{
-    printf("\033[33m[ ");
-    printf("\033[33m%c", wrong_position);
-    printf("\033[33m ]\033[0m");
-}
-void wrong_character(char wrong_character) // printing normal tries
-{
-    printf("\033[0m[ ");
-    printf("\033[0m%c", wrong_character);
-    printf("\033[0m ]\033[0m");
-}
-
 void diff(string guess, string wordle) // indicate differences between guess and wordle
 {
 	for (int i = 0; i < guess.length(); i++) {
