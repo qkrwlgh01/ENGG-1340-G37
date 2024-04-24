42c09cd3f207034f6730d0fe5e690ad61a1a4c70
DearFirefly <159635690+DearFirefly@users.noreply.github.com>
2024-04-24 17:19:00 +0800

    Update header.h

--- header.h
+++ header.h
@@ -6,9 +6,10 @@
 #include <vector>
 
 using namespace std;
-void diff(string guess, string wordle);
 void correct_position(char correct_character);
 void wrong_position(char correct_word);
 void wrong_character(char wrong_character);
+void diff(string guess, string wordle);
+
 
 #endif
