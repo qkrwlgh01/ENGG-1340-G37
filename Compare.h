111b43bd86f5ce4438883d036a8e078da4c18e54
DearFirefly <159635690+DearFirefly@users.noreply.github.com>
2024-04-24 13:22:23 +0800

    Update and rename Compare.h to Difference.h

--- Compare.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMPARE_H
-#define COMPARE_H
-
-void compare(string guess, string wordle);
-
-#endif
---
+++ Difference.h
@@ -0,0 +1,6 @@
+#ifndef DIFFERENCE_H
+#define DIFFERENCE_H
+
+void diff(string guess, string wordle);
+
+#endif
