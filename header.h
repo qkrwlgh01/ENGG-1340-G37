bbb7de3b181fed652724ceed191a1364864c6d95
DearFirefly <159635690+DearFirefly@users.noreply.github.com>
2024-04-24 17:16:04 +0800

    Update header.h

--- header.h
+++ header.h
@@ -2,13 +2,14 @@
 #define HEADER_H
 
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string>
 #include <cctype>
 #include <vector>
 
 using namespace std;
 void diff(string guess, string wordle);
+void correct_position(char correct_character);
+void wrong_position(char correct_word);
+void wrong_character(char wrong_character);
 
 #endif
