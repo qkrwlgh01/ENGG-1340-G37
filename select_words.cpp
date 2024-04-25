2b3d55647263b83e4135b454442124f2ac4913f1
DearFirefly <159635690+DearFirefly@users.noreply.github.com>
2024-04-25 20:05:13 +0800

    Create select_words.cpp

---
+++ select_words.cpp
@@ -0,0 +1,9 @@
+#include <algorithm>
+#include "header.h"
+
+vector<string> select_words(vector<string> words, int word_length, int num_words)
+{
+    string word;
+    vector<string> wordles;
+    for (int i = 0; i < num_words) {
+        word = 
