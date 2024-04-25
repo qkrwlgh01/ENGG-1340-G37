0ce787e84f450c600233dccc2bcd0e440b79e10d
DearFirefly <159635690+DearFirefly@users.noreply.github.com>
2024-04-25 20:16:22 +0800

    Update select_words.cpp

--- select_words.cpp
+++ select_words.cpp
@@ -1,9 +1,23 @@
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include "header.h"
 
+// ramdomly select specified number of words with specified length for the single game
 vector<string> select_words(vector<string> words, int word_length, int num_words)
 {
     string word;
+    int size = words.size();
     vector<string> wordles;
-    for (int i = 0; i < num_words) {
-        word = 
+
+    srand(time(NULL));
+
+    for (int i = 0; i < num_words; i++) {
+        word = words[rand() % size];
+        if (wordles.find(to_lower_case(word)) == -1) { // avoid repeated selection
+            wordles.push_back(to_lower_case(word));
+        }
+    }
+
+    return wordles;
+}
