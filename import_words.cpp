fc56efe6cfb47c5cb0306caa2c39e9adcb4c1fe2
DearFirefly <159635690+DearFirefly@users.noreply.github.com>
2024-04-25 14:02:58 +0800

    Update import_words.cpp

--- import_words.cpp
+++ import_words.cpp
@@ -1,4 +1,26 @@
-#include <iostream>
 #include <fstream>
-#include <cstdlib>
-using namespace std;
+#include "header.h"
+
+vector<string> import_words(int num_letters)
+{
+    string word, filename;
+    vector<string> words;
+    ifstream fin;
+
+    switch(num_letters) // import txt file storing words with corresponding length (name to be declard)
+    {
+        case 5: filename = ""
+        case 7: filename = ""
+        case 9: filename = ""
+        default: filename = ""
+    }
+
+    fin.open(c_str(filename));
+    
+    while(fin >> word) { // extract the whole file
+        words.push_back(word);
+    }
+    fin.close();
+    
+    return words;
+}
