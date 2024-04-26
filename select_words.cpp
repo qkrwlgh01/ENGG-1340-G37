39aea0bf042f6a19a3b337da4124e12f99c52418
ray <159608790+ray0hku@users.noreply.github.com>
2024-04-26 14:34:05 +0800

    Update select_words.cpp

--- select_words.cpp
+++ select_words.cpp
@@ -4,7 +4,7 @@
 #include "header.h"
 
 // ramdomly select specified number of words with specified length for the single game
-vector<string> select_words(const vector<string>& words, int word_length, int num_words)
+vector<string> select_words(const vector<string>& words, int word_length, int 99) // After guessing 99 words correctly continuously
 {
     string word;
     int size = words.size();
@@ -12,7 +12,7 @@ vector<string> select_words(const vector<string>& words, int word_length, int nu
 
     srand(time(NULL));
 
-    for (int i = 0; i < num_words; i++) {
+    for (int i = 0; i < 99; i++) {
         word = words[rand() % size];
         if (wordles.find(to_lower_case(word)) == -1) { // avoid repeated selection
             wordles.push_back(to_lower_case(word));
