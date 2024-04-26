0d8766dcfd455152938dbac9270f280d645f4fd7
DearFirefly <159635690+DearFirefly@users.noreply.github.com>
2024-04-26 14:13:54 +0800

    Update import_header.h

--- import_header.h
+++ import_header.h
@@ -2,6 +2,6 @@
 #define HEADER_H
 
 vector<string> import_words();
-vector<string> select_words(vector<string>& words, int , int);
+vector<string> select_words(vector<string>& words, int word_length, int num_words);
 
 #endif
