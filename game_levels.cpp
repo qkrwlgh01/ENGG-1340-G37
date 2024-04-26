6707c2be9d60c8c4f29215fd9fd08909406c1947
ray <159608790+ray0hku@users.noreply.github.com>
2024-04-26 15:06:10 +0800

    Update game_levels.cpp

--- game_levels.cpp
+++ game_levels.cpp
@@ -1,14 +1,14 @@
 #include "game_levels.h"
 
 // Setup different levels of the game
-GameLevel setup_level(int difficulty) {
+GameLevel setup_level(int level_choice) {
     GameLevel level;
 
     // Default values
     level.word_length = 5; // default word length
     level.num_chances = 6; // default number of chances
 
-    switch (difficulty) {
+    switch (level_choice) {
         case 1:
             level.num_words = 5;
             break;
