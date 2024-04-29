ebf36d97829a7f0ecc83ed846f7d48a657f7c61d
qkrwlgh01 <159628277+qkrwlgh01@users.noreply.github.com>
2024-04-29 13:06:38 +0800

    Update import_words.cpp

--- import_words.cpp
+++ import_words.cpp
@@ -1,17 +1,17 @@
-#include <fstream>
-#include "import_words_header.h"
+#include "import_words_header.h" //Include the header for function declaration
+#include <fstream> //Include for file stream operations
 
-// import all words in word.txt for selection afterwards
-vector<string> import_words()
-{
-    string word;
-    vector<string> words;
-    ifstream fin("word.txt");
-    
-    while(fin >> word) {
-        words.push_back(word);
+// Function that imports words from a file into a vector of strings
+std::vector<std::string> import_words() {
+    std::vector<std::string> words; // Vector to store words
+    std::ifstream file("words.txt"); // Input file stream used to read the file
+    std::string word; // String to store each word read
+
+    // Read words from the file until EOF
+    while (file >> word) {
+        words.push_back(word); // Add each word to the vector
     }
-    fin.close();
 
+    file.close();
     return words;
 }
