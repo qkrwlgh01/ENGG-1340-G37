31c891109c91d22439a3b3c87643be3ae6fefc30
qkrwlgh01 <159628277+qkrwlgh01@users.noreply.github.com>
2024-04-29 13:08:14 +0800

    Update print_char.cpp

--- print_char.cpp
+++ print_char.cpp
@@ -1,22 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "header.h"
+#include "print_char_header.h" // Including the header file for character display functions ensures separation and organization of declarations and definitions.
+#include <cstdio> // Standard library for input and output functions.
 
-void correct_position(char correct_character) //printing right characters with right position, indicated by green colour
-{
-    printf("\033[32m[ "); 
-    printf("\033[32m%c", correct_character);
-    printf("\033[32m ]\033[0m");
+// Displays a character that is correctly guessed and correctly placed
+void correct_position(char correct_character) {
+    printf("\033[32m[ %c ]\033[0m", correct_character);// Uses ANSI escape codes to 
 }
-void wrong_position(char wrong_position) //printing right characters with wrong position, indicated by yellow colour
-{
-    printf("\033[33m[ ");
-    printf("\033[33m%c", wrong_position);
-    printf("\033[33m ]\033[0m");
+
+// Displays a character that is correctly guessed but in the wrong position
+void wrong_position(char wrong_character) {
+    printf("\033[33m[ %c ]\033[0m", wrong_character);
 }
-void wrong_character(char wrong_character) // printing normal tries
-{
-    printf("\033[0m[ ");
-    printf("\033[0m%c", wrong_character);
-    printf("\033[0m ]\033[0m");
+
+// Displays a character that is not in the word at all
+void wrong_character(char wrong_character) {
+    printf("\033[0m[ %c ]\033[0m", wrong_character); // Resets the terminal color before displaying the character.
+
 }
